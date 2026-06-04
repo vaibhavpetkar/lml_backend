d2f02d79dda5ad61e9a7936eb2b32d6a3a9f7806
vaibhavpetkar <vaibhavprakashpetkar@gmail.com>
2026-06-04 21:50:16 +0530

    added basic get route

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
